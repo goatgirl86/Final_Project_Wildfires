5f80c8be152b772e672e96a470be2ffbeffe14dd
Enzo <Zirotti32@gmail.com>
2022-05-21 13:19:55 -0600

    Updating the website prediction outcome

--- data_transfer.py
+++ data_transfer.py
@@ -1,45 +1,26 @@
 import pandas as pd
 import numpy as np
-from imblearn.ensemble import EasyEnsembleClassifier
-from sklearn.preprocessing import StandardScaler
-import json
+import joblib
 
-
-class NumpyEncoder(json.JSONEncoder):
-    """ Special json encoder for numpy types """
-
-    def default(self, obj):
-        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
-                            np.int16, np.int32, np.int64, np.uint8,
-                            np.uint16, np.uint32, np.uint64)):
-            return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32,
-                              np.float64)):
-            return float(obj)
-        elif isinstance(obj, (np.ndarray,)):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
 
 
 def custome_transfer_func(test_data):
 
     
-    # standized scale
-    # Create a StandardScaler instance
-    ss = StandardScaler()
-
-    X = test_data[["state", "discovery_month",
-                   "Temp_pre_7", "Wind_pre_7", "Hum_pre_7"]]
-
-    print("********")
-    print(X)
+    print("*******")
+    print(type(test_data))
+    #X_df = pd.DataFrame([test_data], columns=["state_no", "discovery_month_no", "Temp_pre_7", "Wind_pre_7", "Hum_pre_7"])
+    X = test_data.values
+    
+    
 # Fit the StandardScaler
-    X_scaler = ss.fit_transform(X)
 
+    X_scaler = joblib.load("X_scaler.save")
+    X_scaled = X_scaler.transform(X)
 # Scale the data
-    #X_test_scaled = X_scaler.transform(X_scaler)
-    return(X_scaler)
-    #return(json.dumps(X_scaler, cls=NumpyEncoder))
+    
+    return(X_scaled)
+    
 
 
 
